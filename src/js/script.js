ef60d22332be7e965e8acf9040b1cd010ed59d64
ハルノキ <harunoki.life@gmail.com>
2024-12-18 06:41:05 +0900

    セクション制作（campaign）, スワイパー実装（campaign）

--- src/js/script.js
+++ src/js/script.js
@@ -60,3 +60,66 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
 });
+
+
+/*-------------------------------
+  スワイパー実装（fv）
+-------------------------------*/
+const fvSwiper = new Swiper('.fv__swiper', { //swiperの名前
+  //切り替えのモーション
+  speed: 1000, //表示切り替えのスピード
+  effect: "fade", //切り替えのmotion (※1)
+  allowTouchMove: true, // スワイプで表示の切り替えを有効に
+
+  //最後→最初に戻るループ再生を有効に
+  loop: true,
+  //自動スライドについて
+  autoplay: {
+    delay: 3000, //何秒ごとにスライドを動かすか
+    stopOnLastSlide: false, //最後のスライドで自動再生を終了させるか
+    disableOnInteraction: false, //ユーザーの操作時に止める
+    reverseDirection: false, //自動再生を逆向きにする
+  },
+
+   //表示について
+  centeredSlides: true, //中央寄せにする
+  slidesPerView: 1,
+  // spaceBetween: 30,
+});
+
+/*-------------------------------
+  スワイパー実装（campaign）
+-------------------------------*/
+const campaignSwiper = new Swiper('.campaign__swiper', { //swiperの名前
+  //切り替えのモーション
+  speed: 1000, //表示切り替えのスピード
+  effect: "slide", //切り替えのmotion (※1)
+  allowTouchMove: true, // スワイプで表示の切り替えを有効に
+
+  //最後→最初に戻るループ再生を有効に
+  loop: true,
+  //自動スライドについて
+  autoplay: {
+    delay: 3000, //何秒ごとにスライドを動かすか
+    stopOnLastSlide: false, //最後のスライドで自動再生を終了させるか
+    disableOnInteraction: false, //ユーザーの操作時に止める
+    reverseDirection: false, //自動再生を逆向きにする
+  },
+
+   //表示について
+  centeredSlides: false, //中央寄せにする
+  slidesPerView: "auto",
+  spaceBetween: 24,
+
+  //ナビゲーション
+  navigation: {
+    prevEl: ".swiper-button-prev", //戻るボタンのclass
+    nextEl: ".swiper-button-next" //進むボタンのclass
+  },
+  //ブレイクポイントによって変える
+  breakpoints: {
+    768: {
+      spaceBetween: 30,
+    }
+  }
+});
